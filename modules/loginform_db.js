afc0df7fa95e925bf36feb6b791b3d8d4a132a95
VIGNESHPRABUT44\User <balajisurya493@gmail.com>
2017-12-20 17:16:34 +0530

    loginform_db

--- modules/loginform_db.js
+++ modules/loginform_db.js
@@ -4,28 +4,29 @@ module.exports =
 {
         userslogin_db: function(response,res,callback){
         console.log("inside login db");
-        var result = false;
+       // var result = false;
         var MongoClient = require('mongodb').MongoClient;
         var url = CONSTANTS.MONGODBURL;
         var email=response.email;
         var password=response.password;
-
+		var t_response=null;
         MongoClient.connect(url, function(err, db) {
           if (err) throw err;
           var myobj = { email:email,password: password };
 
           console.log("obj"+JSON.stringify(myobj));
           
-          db.collection("users").findOne(myobj, function(err, result) {
+          db.collection("users").findOne(myobj,{_id:false}),.toArrayfunction((err, result) {
             
             if (err) 
                 throw err;
+			if(t_response==null){
+				t_response=result;
+				callback(result,res);
+			}
             db.close();
             
-            result = false;
-                
-            callback(result, res);
-          });
+            });
 
         });
 
