9c0c622b74c03801860c53cb82f58178ff658e57
makesh <mahesh.mmk48@gmail.com>
2017-12-20 15:09:53 +0530

    integrated pages

--- index.js
+++ index.js
@@ -24,14 +24,41 @@ app.use(function(req, res, next) {
 });
 
 
+
+
 app.get('/',function(req,res){
   
   console.log("index page");
   //res.send("hello world");
 
-  res.sendFile(path.join(__dirname+'/WebContent/html/homepage.html'));
+  res.sendFile(path.join(__dirname+'/WebContent/html/main_home_page.html'));
   //__dirname : It will resolve to your project folder.
 });
+
+
+//login page
+app.get('/login', function(req, res) {
+    res.sendFile('WebContent/html/user_loginpage.html', {
+        root: __dirname
+    });
+});
+
+
+//register user
+app.get('/signUp', function(req, res) {
+    res.sendFile('WebContent/html/user_signuppage.html', {
+        root: __dirname
+    });
+});
+
+
+//addpost
+app.get('/post', function(req, res) {
+    res.sendFile('WebContent/html/homepage.html', {
+        root: __dirname
+    });
+});
+
 
 
 var server = app.listen(app.get('port'), function () {
