a5bda0cf91f50672e16fc093f3814f964aebda98
makesh <mahesh.mmk48@gmail.com>
2017-12-20 13:07:45 +0530

    updated port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var app = express();
 
 var path    = require("path");
 
+app.set('port', (process.env.PORT || 5000));
+
 
 app.use(express.static('WebContent'));
 app.use(bodyParser.urlencoded());
@@ -32,39 +34,9 @@ app.get('/',function(req,res){
 });
 
 
-var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
+var server = app.listen(app.get('port'), function () {
+   var host = server.address().address;
+   var port = server.address().port;
    
-   console.log("Example app listening at http://%s:%s", host, port)
-})
-
-
-app.get('/signup', function(req, res) {
-    res.sendFile('WebContent/html/user_signuppage.html', {
-        root: __dirname
-    });
+   console.log("Example app listening at http://%s:%s", host, port);
 });
-app.post('/SignUpUser', function (req, res) {
-   //fetch user details
-   //validate data
-   //store data to DB
-   //send response
-   
-   console.log("inside-----"+req);
-   console.log("inside signup"+JSON.stringify(req.body));
-   var email = req.body.email;
-   var username = req.body.username;
-   var password = req.body.password;
-   var response = {"username":username,"password":password,"email":email}
-   signupform.usersignup_db(response, res,function(result, response1){
-	   console.log("hey"+result);
-		if(result){
-			response1.send({"status":"success"});
-		}
-		else 
-			response1.send({"status":"error"});
-	   
-   });
-	
-})
